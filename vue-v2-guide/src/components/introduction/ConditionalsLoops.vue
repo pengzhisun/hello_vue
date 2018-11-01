f84ab10e7b60d7618e6c1b80ad02059198e35db9
Pengzhi Sun <pengzhisun@live.com>
2018-11-02 00:35:14 +0800

    add loops guide

--- vue-v2-guide/src/components/introduction/ConditionalsLoops.vue
+++ vue-v2-guide/src/components/introduction/ConditionalsLoops.vue
@@ -7,18 +7,33 @@
       </span>
       <pre>document.$subComponent.$data.sample_01.seen = {{!sample_01.seen}}</pre>
     </div>
+    <div>
+      <h3>Sample 02</h3>
+      <ol>
+        <li v-for="todo in sample_02.todos" :key="todo.text">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    title: "Conditional And Loops"
+    title: 'Conditional And Loops'
   },
   data() {
     return {
       sample_01: {
         seen: true
+      },
+      sample_02: {
+        todos: [
+          { text: 'Learn JavaScript' },
+          { text: 'Learn Vue' },
+          { text: 'Build something awesome' }
+        ]
       }
     };
   },
